26060e090c87803cb942b06c61b60e917e93646f
Karim Al-Sherif <stronciy@gmail.com>
2026-03-20 20:34:18 +0200

    fix

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,7 +5,6 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
-    initShadowDb: "CREATE EXTENSION IF NOT EXISTS postgis;",
   },
   datasource: {
     url: process.env.DATABASE_URL ?? "postgresql://postgres:postgres@localhost:5432/postgres?schema=public",
