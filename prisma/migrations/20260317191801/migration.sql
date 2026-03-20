05097b5f3ab8d6fc262c4225b06ccdf6cdf36d68
Karim Al-Sherif <stronciy@gmail.com>
2026-03-20 21:01:15 +0200

    dd

--- prisma/migrations/20260317191801/migration.sql
+++ prisma/migrations/20260317191801/migration.sql
@@ -1,8 +1,1 @@
--- DropIndex
-DROP INDEX "idx_fields_centroid_geo_gist";
-
--- DropIndex
-DROP INDEX "idx_orders_location_geo_gist";
-
--- DropIndex
-DROP INDEX "idx_performer_settings_base_geo_gist";
+-- Drop spatial indexes removed (as they were never created)
